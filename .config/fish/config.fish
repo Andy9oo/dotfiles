f1ce4955f79766a73a972a0e102bd2e61d8347da
Andrew Cullis <acullis68@gmail.com>
2021-04-20 22:26:43 +0200

    Cleaned up i3 config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,11 @@ set -g -x fish_greeting ''
 
 # Setup aliases
 alias c "code . && exit"
+alias o "xdg-open"
 
 # PATH variables
 set PATH /home/andrew/Code/work/tools/bin $PATH
 set PATH /home/andrew/go/bin $PATH
+
+# Exports
+set -x ANDROID_HOME /home/andrew/android/sdk
